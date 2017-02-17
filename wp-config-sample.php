8b72801f2a5ca9d40aea822a86fb50954d868910
giulia <giulia@Mac-mini.local>
2017-02-17 14:32:40 +1100

    test

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -25,19 +25,19 @@
 
 // ** Impostazioni MySQL - È possibile ottenere queste informazioni dal proprio fornitore di hosting ** //
 /** Il nome del database di WordPress */
-define('DB_NAME', 'nome_del_database_qui');
+define('DB_NAME', '');
 
 /** Nome utente del database MySQL */
-define('DB_USER', 'nome_utente_qui');
+define('DB_USER', '');
 
 /** Password del database MySQL */
-define('DB_PASSWORD', 'password_qui');
+define('DB_PASSWORD', '');
 
 /** Hostname MySQL  */
 define('DB_HOST', 'localhost');
 
 /** Charset del Database da utilizzare nella creazione delle tabelle. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** Il tipo di Collazione del Database. Da non modificare se non si ha idea di cosa sia. */
 define('DB_COLLATE', '');
@@ -51,14 +51,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'Mettere la vostra frase unica qui');
-define('SECURE_AUTH_KEY',  'Mettere la vostra frase unica qui');
-define('LOGGED_IN_KEY',    'Mettere la vostra frase unica qui');
-define('NONCE_KEY',        'Mettere la vostra frase unica qui');
-define('AUTH_SALT',        'Mettere la vostra frase unica qui');
-define('SECURE_AUTH_SALT', 'Mettere la vostra frase unica qui');
-define('LOGGED_IN_SALT',   'Mettere la vostra frase unica qui');
-define('NONCE_SALT',       'Mettere la vostra frase unica qui');
+define('AUTH_KEY',         'T/ ij9G0K &apS0W;5,(?lO[r7bpa-2Y-X_29Bas>Q`+k+$h*z4w3*19*b@ n})A');
+define('SECURE_AUTH_KEY',  '55af^wt}y-^FbiE`; 8yi530o=vY#[`+w-!54jcVJd{sN%v!w8e$S^g[mjNcvYgO');
+define('LOGGED_IN_KEY',    'I$R,lzE7SbEQkHClq-?tDWqY_i|8jO{;G|SxqZOQ(Y]GjCEH(Dj22.C/=Cc>PW3j');
+define('NONCE_KEY',        'f2TH`Wii2P&`=v@7SlJMyBz+ g`9C3%S5EtvA4GK`4Q`09AHqd^WZfIaEMqU;_gD');
+define('AUTH_SALT',        'D ;uZXVJQz_4KD9[}%wBjx{dFykCjrqZ5Yoi*<iS}|%M)8X9D`EaF,<<E,~-~+%y');
+define('SECURE_AUTH_SALT', '!7 F-Vh&NgUIg5+%$HmFCHYEX=h2c~5OY;7TT@Tzc6rxDm9/jSo`+_Swx!8v=.9n');
+define('LOGGED_IN_SALT',   'K0$v5)Eb46F-R&+<)T3wvzwBUX$GzO^HVJHp<geQ#GcOt-O{{!_FzaC #a>:LXCh');
+define('NONCE_SALT',       '=w~U@4waae,o0o[Xjf]u6frhUq61Eyj6+F0Y23dSUq@evVs] B9kp o}k4]%eFKy');
 
 /**#@-*/
 
@@ -69,7 +69,7 @@ define('NONCE_SALT',       'Mettere la vostra frase unica qui');
  * fornendo a ciascuna installazione un prefisso univoco.
  * Solo numeri, lettere e sottolineatura!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'or32_';
 
 /**
  * Per gli sviluppatori: modalità di debug di WordPress.
